8730e7a1bcce432bab0b205d90c56670d0173426
Louis <brousselouis@hotmail.com>
2024-01-11 16:17:03 +0100

    Router #1

---
+++ frontend/src/router/index.js
@@ -0,0 +1,27 @@
+
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Ticketform from '/src/views/Ticketform.vue'
+
+const routes = [
+
+
+   {
+      path: '/',
+      component: Ticketform,
+   },
+
+   {
+      path: '/recap/:ticketId',
+      component: () => import('/src/views/Recap.vue'),
+      props: true,
+   },
+
+]
+
+const router = createRouter({
+   history: createWebHistory('/'),
+   routes,
+})
+
+export default router
---
+++ frontend/src/views/Recap.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        Recap
+    </div>
+</template>
+
