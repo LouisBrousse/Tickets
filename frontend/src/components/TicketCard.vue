e268f7add105e21485aefb1cb3f4015142d8fabe
Louis <brousselouis@hotmail.com>
2024-01-12 11:26:49 +0100

    partie 5.1

--- frontend/src/components/TicketCard.vue
+++ frontend/src/components/TicketCard.vue
@@ -1,26 +1,57 @@
-<div id="card" v-for="ticket in listetickets" class="max-w-md rounded overflow-hidden shadow-lg bg-white">
-    <div class="text-left px-2 py-2">
-        <span class="inline-block rounded-full px-2 py-1 text-sm font-semibold mr-2 w-28 text-center"
-        :class="{
-        'bg-yellow-200': ticket.category === 'furniture',
-        'bg-orange-200 ': ticket.category === 'computer',
-        'bg-purple-200': ticket.category === 'other'
-        }">{{ CATEGORIES[ticket.category]}}</span>
-    </div>
+<template>
+        
+<div class="max-w-md rounded overflow-hidden shadow-lg bg-white">
+                <div id="category" class="text-left px-2 py-2">
+                <span class="inline-block rounded-full px-2 py-1 text-sm font-semibold mr-2 w-28 text-center"
+                      :class="{
+                         'bg-yellow-200': ticket.category === 'furniture',
+                         'bg-orange-200 ': ticket.category === 'computer',
+                         'bg-purple-200': ticket.category === 'other'
+                      }">{{ CATEGORIES[ticket.category]}}</span>
+                </div>
+ 
+                <div id="id" class="font-bold text-xl px-6  mb-2">Id# : <a>{{ticket.id}}</a></div>
+             
+                <div id="date" class="px-6 py-4">
+                    <div class="text-base mb-2">Date : <a>{{formatDateTime(ticket.created_at)}}</a></div>
+                </div>
+                <div id="priority" class="px-2 py-4 text-right">
+                    <span class="inline-block rounded-full px-2 py-1 text-sm font-semibold mr-2 w-28 text-center"
+                        :class="{
+                            'bg-blue-700 text-green-200': ticket.priority === 'low',
+                            'bg-green-500 text-green-100': ticket.priority === 'normal',
+                            'bg-red-700 text-red-200': ticket.priority === 'high'
+                        }">{{ PRIORITIES[ticket.priority]}}</span>
+                </div>     
+</div> 
+                    
+ 
+    
+  </template>
+  
+  <script setup>
+ import { ref, onMounted, defineProps } from 'vue';
+ import { format } from 'date-fns';
+ import { CATEGORIES, PRIORITIES } from '../constants';
+ 
+ const listetickets = ref({});
+ 
+ const formatDateTime = (dateTime) => {
+   return format(new Date(dateTime), 'dd/MM/yyyy HH:mm:ss');
+ };
+ 
+ const props = defineProps({
+    ticketId: {
+       type: [Number, String],
+       required: true,
+    },
+ })
 
-    <div class="font-bold text-xl px-6  mb-2">Id# : <a>{{ticket.id}}</a></div>
-
-    <div class="px-6 py-4">
-        <div class="text-base mb-2">Email: <a>{{ticket.email}}</a></div>
-        <div class="text-base mb-2">Date : <a>{{formatDateTime(ticket.created_at)}}</a></div>
-    </div>
-    <div class="px-2 py-4 text-right">
-        <span class="inline-block rounded-full px-2 py-1 text-sm font-semibold mr-2 w-28 text-center"
-        :class="{
-        'bg-blue-700 text-green-200': ticket.priority === 'low',
-        'bg-green-500 text-green-100': ticket.priority === 'normal',
-        'bg-red-700 text-red-200': ticket.priority === 'high'
-        }">{{ PRIORITIES[ticket.priority]}}</span>
-    </div>
-
-</div>
+ onMounted(async () => {
+     const response = await fetch(`/api/ticket`)
+     console.log("liste", response)
+     listetickets.value= await response.json()
+     console.log("liste", listetickets)
+  })
+  </script>
+  
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,10 +2,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Ticketform from '/src/views/Ticketform.vue'
+import Ticketslist2 from '/src/views/ticketslist2.vue'
 import Ticketslist from '/src/views/ticketslist.vue'
+import TicketCard from '/src/components/TicketCard.vue'
 
 const routes = [
-
 
    {
       path: '/',
@@ -17,12 +18,26 @@ const routes = [
       component: () => import('/src/views/ticketRecap.vue'),
       props: true,
    },
+   
 
-   
-   
    {
       path: '/ticketslist',
-      component: Ticketslist,
+      component: Ticketslist, 
+      // children: [
+      //    {
+      //       path: ':ticketId',
+            
+      //       component: TicketDetail,
+      //       // component: () => import('/src/components/CityDetail.vue'),
+      //       props: true
+      //    },
+      // ],
+      
+   },
+
+   {
+      path: '/ticketslist2',
+      component: Ticketslist2,
    },
 
 ]
